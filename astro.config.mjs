a405a16cec8a200f3212478dbb5e8d60a2ee4c86
Darren Kim <darren.minsoo.kim@gmail.com>
2025-12-30 05:46:45 -0500

    install tailwindcss via astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   // 'hybrid' 대신 이제 기본값인 'static'을 사용합니다.
   // 어댑터가 설정되어 있으면 static 모드 내에서도 SSR 페이지를 만들 수 있습니다.
   output: 'static',
+
   adapter: node({
     mode: 'standalone',
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
