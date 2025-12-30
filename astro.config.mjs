f190aac72c7023126fe1f07fa948ce45f6868cb2
Darren Kim <darren.minsoo.kim@gmail.com>
2025-12-29 19:55:10 -0500

    set basic structure on pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 'hybrid' 대신 이제 기본값인 'static'을 사용합니다.
+  // 어댑터가 설정되어 있으면 static 모드 내에서도 SSR 페이지를 만들 수 있습니다.
+  output: 'static',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
